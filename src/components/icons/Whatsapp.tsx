b70cf97a23b953494e49f6f06c4d509e94df80c3
alexmp2602 <alexmp.2602@gmail.com>
2024-03-13 23:28:29 -0300

    Actualizar estilos y contenido del componente de ubicación, enlaces en el encabezado y el pie de página, y ajustes en estilos y bibliotecas de animación.

--- src/components/icons/Whatsapp.tsx
+++ src/components/icons/Whatsapp.tsx
@@ -1,19 +1,37 @@
-export default function WhatsappIcon(props: React.ComponentProps<"svg">) {
+"use client"
+
+import React from 'react';
+
+interface WhatsappIconProps extends React.SVGProps<SVGSVGElement> {
+  className?: string;
+}
+
+const WhatsappIcon: React.FC<WhatsappIconProps> = ({ className, ...rest }) => {
+  const handleWhatsAppClick = (e: React.MouseEvent<SVGSVGElement>) => {
+    e.preventDefault();
+    const whatsappUrl = 'https://wa.me/+542615188484?text=¡Hola!%20Me%20gustaría%20saber%20más%20sobre%20sus%20servicios.';
+    window.open(whatsappUrl, '_blank');
+  };
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      onClick={handleWhatsAppClick}
+      {...rest}
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
       <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
     </svg>
   );
 }
+
+export default WhatsappIcon;
